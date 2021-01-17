ec0c69d042b120a3a44aee3305428c26b27e401a
Matus Kempa <matus.kempa@gmail.com>
2021-01-17 15:50:10 +0100

    Export where builder and building functions

--- packages/utils/lib/index.js
+++ packages/utils/lib/index.js
@@ -2,3 +2,4 @@ export { default as format } from './format';
 export { default as misc } from './misc';
 export { default as species } from './species';
 export { default as where } from './where';
+export * from './builders/where-builder';
--- packages/utils/lib/where.js
+++ packages/utils/lib/where.js
@@ -3,6 +3,10 @@ import {
   eq, and,
 } from './builders/where-builder';
 
+/**
+ * Creates where equal on every property in data
+ * @param {object} data 
+ */
 function whereDataAll(data) {
   const andItems = Object.keys(data)
     .filter((k) => !!data[k])
