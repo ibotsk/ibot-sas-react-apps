73ab8e7b3a4feb62d0e2c3db3e1255364abb1c5e
Matus Kempa <matus.kempa@gmail.com>
2021-03-05 15:45:59 +0100

    Minor restyle of skeleton table

--- packages/slovplantlist-web-app/src/components/segments/Common/SkeletonTable.js
+++ packages/slovplantlist-web-app/src/components/segments/Common/SkeletonTable.js
@@ -1,9 +1,10 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 
-import { Table, TableBody, TableRow } from '@material-ui/core';
+import {
+  Table, TableBody, TableRow, TableCell,
+} from '@material-ui/core';
 import Skeleton from '@material-ui/lab/Skeleton';
-import { makeStyles } from '@material-ui/core/styles';
 
 import config from 'config';
 
@@ -11,32 +12,18 @@ const {
   pagination: { rowsPerPageOptions },
 } = config;
 
-const useStyle = makeStyles(() => ({
-  skeletonTable: {
-    '& tr': {
-      height: 53,
-    },
-  },
-}));
-
-const SkeletonTable = () => {
-  const classes = useStyle();
-
-  return (
-    <Table className={classes.skeletonTable}>
-      <TableBody>
-        {[...Array(rowsPerPageOptions[0] + 2)].map((_, i) => (
-          <TableRow key={i}>
-            <Skeleton
-              variant="rect"
-              height={50}
-              component="td"
-            />
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  );
-};
+const SkeletonTable = () => (
+  <Table>
+    <TableBody>
+      {[...Array(rowsPerPageOptions[0] + 2)].map((_, i) => (
+        <TableRow key={i}>
+          <TableCell>
+            <Skeleton />
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+);
 
 export default SkeletonTable;
