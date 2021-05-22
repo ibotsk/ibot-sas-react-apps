6b90878d66b35342e98c914f6c24dec0f9749441
Matus Kempa <matus.kempa@gmail.com>
2021-05-22 14:21:16 +0200

    Remade login page

--- packages/slovplantlist-admin-js/src/components/pages/Login.js
+++ packages/slovplantlist-admin-js/src/components/pages/Login.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import {
-  Redirect,
-} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
-import {
-  Grid, Col,
-  Button,
-  Form, FormGroup, FormControl, ControlLabel,
-} from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 
-import { PageTitle } from '@ibot/components';
-
-import PropTypes from 'prop-types';
-
-import { usersFacade } from 'facades';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 import {
   setAuthenticated as setAuthenticatedAction,
@@ -23,38 +19,53 @@ import {
   unsetUser as unsetUserAction,
 } from 'actions';
 
+import { usersFacade } from 'facades';
 import config from 'config/config';
+import { useLocation } from 'react-router';
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
+const { mappings: { userRole: userRoleConfig } } = config;
 
-    const { unsetAuthenticated, unsetUser } = this.props;
-    unsetAuthenticated();
-    unsetUser();
-    this.state = {
-      username: '',
-      password: '',
-      redirectToReferrer: false,
-    };
-  }
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-  validateForm = () => {
-    const { username, password } = this.state;
-    return username.length > 0 && password.length > 0;
-  };
+const Login = () => {
+  const classes = useStyles();
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.id]: event.target.value,
-    });
-  }
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirectToReferrer, setRedirectToReferer] = useState(false);
 
-  handleSubmit = async (e) => {
+  const dispatch = useDispatch();
+  const {
+    state: locationState = {
+      from: { pathname: '/' },
+    },
+  } = useLocation();
+
+  useEffect(() => {
+    dispatch(unsetAuthenticatedAction());
+    dispatch(unsetUserAction());
+  }, [dispatch]);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const { username, password } = this.state;
-    const { setAuthenticated, setUser } = this.props;
-
     // stop here if form is invalid
     if (!(username && password)) {
       return;
@@ -74,80 +85,72 @@ class Login extends Component {
 
     const userRole = roles[0]
       ? roles[0].name
-      : config.mappings.userRole.author.name;
+      : userRoleConfig.author.name;
 
-    setAuthenticated(accessToken);
-    setUser(userId, userRole, userGeneraIds, username);
-    this.setState({ redirectToReferrer: true });
+    dispatch(setAuthenticatedAction(accessToken));
+    dispatch(setUserAction(userId, userRole, userGeneraIds, username));
+    setRedirectToReferer(true);
+  };
+
+  const { from } = locationState;
+  if (redirectToReferrer === true) {
+    return <Redirect to={from} />;
   }
 
-  render() {
-    const { location } = this.props;
-    const { from } = location.state || { from: { pathname: '/' } };
-    const { username, password, redirectToReferrer } = this.state;
-
-    if (redirectToReferrer === true) {
-      return <Redirect to={from} />;
-    }
-
-    return (
-      <div id="login-page">
-        <PageTitle title="Login - Slovplantlist" />
-        <Grid>
-          <Col xs={12} md={4} mdOffset={4}>
-            <h2>Login</h2>
-            <Form onSubmit={this.handleSubmit}>
-              <FormGroup controlId="username" bsSize="large">
-                <ControlLabel>Username</ControlLabel>
-                <FormControl
-                  autoFocus
-                  type="text"
-                  value={username}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
-              <FormGroup controlId="password" bsSize="large">
-                <ControlLabel>Password</ControlLabel>
-                <FormControl
-                  type="password"
-                  value={password}
-                  onChange={this.handleChange}
-                />
-              </FormGroup>
-              <Button
-                block
-                bsSize="large"
-                disabled={!this.validateForm()}
-                type="submit"
-                bsStyle="primary"
-              >
-                Login
-              </Button>
-            </Form>
-          </Col>
-        </Grid>
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h4">
+          SlovPlantList Admin
+        </Typography>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h2" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Username"
+            name="username"
+            autoComplete="off"
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="off"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleSubmit}
+          >
+            Sign In
+          </Button>
+        </form>
       </div>
-    );
-  }
-}
-
-export default connect(null, {
-  setAuthenticated: setAuthenticatedAction,
-  unsetAuthenticated: unsetAuthenticatedAction,
-  setUser: setUserAction,
-  unsetUser: unsetUserAction,
-})(Login);
-
-Login.propTypes = {
-  unsetAuthenticated: PropTypes.func.isRequired,
-  unsetUser: PropTypes.func.isRequired,
-  setAuthenticated: PropTypes.func.isRequired,
-  setUser: PropTypes.func.isRequired,
-  location: PropTypes.shape({
-    state: PropTypes.shape({
-      from: PropTypes.shape({
-        pathname: PropTypes.string,
-      }),
-    }),
-  }).isRequired,
+    </Container>
+  );
 };
+
+export default Login;
