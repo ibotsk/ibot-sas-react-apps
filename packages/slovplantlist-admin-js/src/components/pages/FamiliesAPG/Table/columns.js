dca649184abe9bc428b1d839bfbeae39df5f35ad
Matus Kempa <matus.kempa@gmail.com>
2021-06-07 20:25:53 +0200

    FamiliesAPG page with MUI data grid

---
+++ packages/slovplantlist-admin-js/src/components/pages/FamiliesAPG/Table/columns.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import Can from 'components/segments/auth/Can';
+import Button from '@material-ui/core/Button';
+
+import DoneIcon from '@material-ui/icons/Done';
+import ClearIcon from '@material-ui/icons/Clear';
+
+export const columns = (role, handleShowModal) => [
+  {
+    field: 'id',
+    headerName: 'ID',
+    flex: 0.8,
+  },
+  {
+    field: 'action',
+    headerName: 'Actions',
+    flex: 1,
+    renderCell: (p) => (
+      <Can
+        role={role}
+        perform="familyAPG:edit"
+        yes={() => (
+          <Button
+            size="small"
+            color="primary"
+            onClick={() => handleShowModal(p.row.id)}
+          >
+            Edit
+          </Button>
+        )}
+      />
+    ),
+    sortable: false,
+    filterable: false,
+  },
+  {
+    field: 'checkedTimestamp',
+    headerName: 'Checked',
+    flex: 1,
+    renderCell: (p) => (p.value ? (
+      <DoneIcon />
+    ) : (
+      <ClearIcon />
+    )),
+    sortable: false,
+    filterable: false,
+  },
+  {
+    field: 'name',
+    headerName: 'Name',
+    flex: 2,
+    sort: 'asc',
+  },
+  {
+    field: 'vernacular',
+    headerName: 'Vernacular',
+    flex: 2,
+  },
+];
+
+export const defaultSortModel = [{
+  field: 'name',
+  sort: 'asc',
+}];
