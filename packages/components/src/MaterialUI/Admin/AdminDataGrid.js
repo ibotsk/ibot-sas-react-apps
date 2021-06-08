74beecec1b75a47248766f573ef06b5178904690
Matus Kempa <matus.kempa@gmail.com>
2021-06-08 20:01:27 +0200

    Toolbar added to data grid

--- packages/components/src/MaterialUI/Admin/AdminDataGrid.js
+++ packages/components/src/MaterialUI/Admin/AdminDataGrid.js
@@ -1,29 +1,41 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { DataGrid, GridOverlay } from '@material-ui/data-grid';
+import {
+  DataGrid, GridOverlay,
+  GridToolbarColumnsButton, GridToolbarContainer, GridToolbarFilterButton,
+} from '@material-ui/data-grid';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import { Paper } from '@material-ui/core';
 
-function CustomLoadingOverlay() {
-  return (
-    <GridOverlay>
-      <div style={{ position: 'absolute', top: 0, width: '100%' }}>
-        <LinearProgress />
-      </div>
-    </GridOverlay>
-  );
-}
+const CustomLoadingOverlay = () => (
+  <GridOverlay>
+    <div style={{ position: 'absolute', top: 0, width: '100%' }}>
+      <LinearProgress />
+    </div>
+  </GridOverlay>
+);
+
+const CustomToolbar = () => (
+  <GridToolbarContainer>
+    <GridToolbarColumnsButton />
+    <GridToolbarFilterButton />
+  </GridToolbarContainer>
+);
 
 const AdminDataGrid = (props) => (
-  <DataGrid
-    pagination
-    paginationMode="server"
-    sortingOrder={['desc', 'asc']}
-    filterMode="server"
-    components={{
-      LoadingOverlay: CustomLoadingOverlay,
-    }}
-    {...props}
-  />
+  <Paper style={{ width: '100%', height: '100%' }}>
+    <DataGrid
+      pagination
+      paginationMode="server"
+      sortingOrder={['desc', 'asc']}
+      filterMode="server"
+      components={{
+        LoadingOverlay: CustomLoadingOverlay,
+        Toolbar: CustomToolbar,
+      }}
+      {...props}
+    />
+  </Paper>
 );
 
 export default AdminDataGrid;
